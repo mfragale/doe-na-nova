d0fc4b55bc0a3f5c5260d930ec786c73ceaffbcf
Mauricio Fragale <mfragale@gmail.com>
2022-03-23 10:26:53 -0300

    Update part-login-and-register.php

--- includes/parts/part-login-and-register.php
+++ includes/parts/part-login-and-register.php
@@ -1,14 +1,26 @@
-	<div class="card mt-5 mb-5">
-		<div class="card-body">
-			<div class="row">
-				<div class="col">
-					<p><?php _e('Not registered yet?', 'doenanova'); ?></p>
-					<a class="btn btn-primary" href="<?php echo esc_url(wp_registration_url()); ?>"><?php _e('Register', 'doenanova'); ?></a>
+<div class="card my-5 p-5">
+	<div class="card-body">
+		<div class="row">
+			<div class="col text-center">
+				<h1>Doe com cartão</h1>
+			</div>
+		</div>
+		<div class="row my-3">
+			<div class="col text-center">
+				<p class="text-muted">Para doar utilizando seu cartão de crédito ou débito, primeiro se cadastre ou então faça seu login.</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col mb-sm-3">
+				<div class="d-grid gap-2">
+					<a class="btn btn-primary btn-lg" href="<?php echo esc_url(wp_registration_url()); ?>"><?php _e('Register', 'doenanova'); ?></a>
 				</div>
-				<div class="col">
-					<p><?php _e('Login to make a donation.', 'doenanova'); ?></p>
-					<a class="btn btn-outline-secondary" href="<?php echo esc_url(wp_login_url(home_url())); ?>"><?php _e('Login', 'doenanova'); ?></a>
+			</div>
+			<div class="col">
+				<div class="d-grid gap-2">
+					<a class="btn btn-outline-secondary btn-lg" href="<?php echo esc_url(wp_login_url(home_url())); ?>"><?php _e('Login', 'doenanova'); ?></a>
 				</div>
 			</div>
 		</div>
 	</div>
+</div>
