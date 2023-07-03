ddd24bbcc040dd83e57cc1b59c6d26df2cc992b9
Mauricio Fragale <mfragale@gmail.com>
2023-07-03 11:36:52 -0300

    Update register-js.php

--- includes/register-js.php
+++ includes/register-js.php
@@ -17,7 +17,7 @@ function doe_na_nova_form_js()
 	wp_register_script(
 		'my-functions-doe-js',
 		plugin_dir_url(__FILE__) . 'js/dist/myfunctions-min.js',
-		array('jquery'),
+		array('jquery', 'wp-i18n'),
 		'1.1',
 		true
 	);
